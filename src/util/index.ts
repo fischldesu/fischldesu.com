c43a141db9e25675d14bb01835988986b68be742
fischldesu <fischldesu@outlook.com>
2025-05-31 06:31:36 +0800

    add Music function AppMusic: stp 1 of 2 framework optimize

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,8 +1,19 @@
-export function ShuffleArray<T>(array: T[]): T[] {
+import musicMetaReader, { type MusicMetaData } from './MP3MetaReader';
+
+function ShuffleArray<T>(array: T[]): T[] {
   const arr = array.slice();
   for (let i = arr.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [arr[i], arr[j]] = [arr[j], arr[i]]; // 交换元素
   }
   return arr;
+}
+
+export {
+  musicMetaReader,
+  ShuffleArray
+};
+
+export type {
+  MusicMetaData
 }
